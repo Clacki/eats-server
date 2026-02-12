fb536b4c752a7d29b8907ee584208e98833c1ccf
Clacki <skthk6613@naver.com>
2026-02-12 20:59:01 +0900

    3

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
 
   next();
 });
